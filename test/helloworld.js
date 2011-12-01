6c82a24db5e202af96c52c07f9307b0c8e28b156
Fabian M <mail.fabianm@gmail.com>
2011-12-01 09:21:31 +0100

    Updated Readme.

---
+++ test/helloworld.js
@@ -0,0 +1,17 @@
+/**
+ * Hello world for NodeLog.
+ *
+ * @author Fabian M.
+ */
+var logger = require('../lib/logger.js').getLogger("HelloWorldService");
+
+logger.severe("Hello world");
+logger.warning("Hello world");
+logger.info("Hello world");
+logger.config("Hello world");
+
+// These messages aren't shown because the levelValue is higher that the 
+// 	values of FINE, FINER and FINEST.
+logger.fine("Hello world");
+logger.finer("Hello world");
+logger.finest("Hello world");
