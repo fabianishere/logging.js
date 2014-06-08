7b7d6cfdcfb86476d756a96ca902b4828fb1e654
Fabian M <mail.fabianm@gmail.com>
2014-06-08 15:21:14 +0200

    Update example and configuration system

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -1,2 +1,8 @@
-var logger = require("../lib/logging").get(module);
-logger.info("Hello World!");
+var logging = require("../lib/logging"), logger = logging.get(module).configure(logging.BasicConfiguration);
+
+function main() {
+	logger.info("Hello World!");
+	logger.severe("Help!");
+	logger.warning("Test {0} {1} {2}", 1, 2, 3);
+}
+main();
